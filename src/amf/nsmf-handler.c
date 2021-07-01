2257a0e613fd229a21a24032350f23030fb0b1ac
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-06-30 20:24:24 -0400

    Update nsmf-handler.c

--- src/amf/nsmf-handler.c
+++ src/amf/nsmf-handler.c
@@ -661,25 +661,6 @@ int amf_nsmf_pdusession_handle_update_sm_context(
         return OGS_ERROR;
     }
 
-    char *doc_json;
-    char *imsi_str = sess->amf_ue->supi;
-    imsi_str += 5;
-    //uintmax_t imsi_int = strtoumax(imsi_str, NULL, 10);
-    //int imsi_int = atoi(imsi_str);
-
-    asprintf(&doc_json, "{\"_id\": \"%s\", \"%s\":{\"sm-context-ref\": \"%s\", \"pdu-session-id\": \"%d\"}}", imsi_str, sess->amf_ue->supi, sess->sm_context_ref, recvmsg->SmContextCreatedData->pdu_session_id);
-
-    int rv;
-    rv = insert_data_to_db("AMF", "create", imsi_str, doc_json);
-    if (rv != OGS_OK)
-    {
-        ogs_error("PCS Error while inserting data to MongoDB for supi [%s]", sess->amf_ue->supi);
-    }
-    else
-    {
-        ogs_info("PCS Successfully inserted data to MongoDB for supi [%s]", sess->amf_ue->supi);
-    }
-
     return OGS_OK;
 }
 
