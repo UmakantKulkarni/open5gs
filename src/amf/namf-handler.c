d206c4203324eee09f466a92b46a3ba04c990772
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-10-09 12:53:37 -0400

    fix5

--- src/amf/namf-handler.c
+++ src/amf/namf-handler.c
@@ -27,7 +27,7 @@
 #include "mongoc.h"
 #include "parson.h"
 #include <pthread.h>
-#include "../../lib/core/pcs-thread-pool.h"
+#include "pcs-thread-pool.h"
 
 int amf_namf_comm_handle_n1_n2_message_transfer(
         ogs_sbi_stream_t *stream, ogs_sbi_message_t *recvmsg, pcs_fsm_struct_t *pcs_fsmdata)
